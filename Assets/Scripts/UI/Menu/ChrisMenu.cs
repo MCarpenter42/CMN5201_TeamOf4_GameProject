acf0272f32fdb1ca347c4c262c198dd56db5eb61
Chris <100540uk@saeinstitute.edu>
2022-06-10 14:53:27 +0100

    Example MainMenu
    1st commit of menu

---
+++ Assets/Scripts/UI/Menu/ChrisMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChrisMenu : UI
+{
+    public void Quit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
